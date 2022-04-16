d6349bfa39c353b978e8162c51cec9e76024469a
Sumedha Kanthamneni <skanthamneni@g.ucla.edu>
2022-04-16 05:27:25 +0000

    Addressed Bug in Integration Tests (tests/integration_tests.sh) - Previously, the background processes for the local server test weren't be killed - Now for each local server test, the local server bg process is run at the beginning and killed at the end
    Updated curl in Integration Tests (test/integration_tests.sh)
    - Added -S which will output any error messages from curl
    - Note: since -s is also specified, progress bars won't be outputed, only error messages
    - Changed `> <output_file>` to `-o <output_file>`
    
    Added logic for exit code (test/integration_tests.sh)
    - If any of the tests fail, the bash script will now exit with code 1
    - This is useful for adding the integration test script to ctest/make test
    - Now if one of the integration tests fail, it exits with code 1, and make test indicates that the integration tests failed
    
    Moved setup logic for integration tests into a function (test/integration_tests.sh)
    
    Added Integration Tests to ctest / make test (CMakeLists.txt)
    
    Change-Id: Ieb0b596c605a774b49f4380c52aa3130597fd4d4

--- tests/integration_tests.sh
+++ tests/integration_tests.sh
@@ -4,26 +4,43 @@ GCP_IP=35.203.165.222
 GCP_PORT_NUM=80
 LOCAL_PORT_NUM=8080
 
+# setup
+function run_setup() {
+	# enter build directory to make server
+	cd ../build
+	echo "building server..."
+	make server &>/dev/null
+	cd ../tests
+}
+
+# start local server as background process
+function start_local_server() {
+	../build/bin/server local_test_config &>/dev/null &
+	local_server_pid=$!
+}
+
 # testing functions
 function local_response_header_test() {
-	../build/bin/server local_test_config &>/dev/null &
-	curl -s -I http://localhost:${LOCAL_PORT_NUM} > /tmp/actual
+	start_local_server
+	curl -s -S -I http://localhost:${LOCAL_PORT_NUM} -o /tmp/actual
+	kill ${local_server_pid}
 	diff expected_local_header /tmp/actual
 }
 
 function local_full_response_test() {
-	../build/bin/server local_test_config &>/dev/null &
-	curl -s -i http://localhost:${LOCAL_PORT_NUM} > /tmp/actual
+	start_local_server
+	curl -s -S -i http://localhost:${LOCAL_PORT_NUM} -o /tmp/actual
+	kill ${local_server_pid}
 	diff expected_local_full /tmp/actual
 }
 
 function gcp_response_header_test() {
-	curl -s -I ${GCP_IP}:${PORT_NUM} > /tmp/actual
+	curl -s -S -I ${GCP_IP}:${PORT_NUM} -o /tmp/actual
 	diff expected_gcp_header /tmp/actual
 }
 
 function gcp_full_response_test() {
-	curl -s -i ${GCP_IP}:${PORT_NUM} > /tmp/actual
+	curl -s -S -i ${GCP_IP}:${PORT_NUM} -o /tmp/actual
 	diff expected_gcp_full /tmp/actual
 }
 
@@ -31,11 +48,8 @@ function gcp_full_response_test() {
 test_list=(local_response_header local_full_response
 	gcp_response_header gcp_full_response)
 
-# enter build directory to make server
-cd ../build
-echo "building server..."
-make server &>/dev/null
-cd ../tests
+# run setup commands
+run_setup
 
 # run tests
 num_tests=${#test_list[@]}
@@ -57,3 +71,10 @@ done
 
 # report how many tests passed
 echo "${passed} of ${num_tests} tests passed"
+
+if [ ${passed} -eq ${num_tests} ]
+then
+	exit 0
+else
+	exit 1
+fi
