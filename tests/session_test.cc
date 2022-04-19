5e30bc72a387125a7001a318d9884f224144ff6b
William Chong <williamchong@g.ucla.edu>
2022-04-19 04:23:35 +0000

    Added MultipleRequests unit test for Session class
    Change-Id: I5b836fd7ce4d443fd7964bdd0256acc337982e49

--- tests/session_test.cc
+++ tests/session_test.cc
@@ -22,3 +22,15 @@ TEST_F(SessionTest, statusCode) {
   EXPECT_EQ(session.gethttpResponse().result_int(), 200);
 }
 
+TEST_F(SessionTest, multipleRequests) {
+  char test_data[] = "http test body data.\n";
+  char test_data2[] = "another http body data test.\n";
+  int success = session.handle_http(test_data, 21);
+  EXPECT_TRUE(success);
+  EXPECT_EQ(session.gethttpResponse().body(), std::string(test_data, 21));
+  EXPECT_EQ(session.gethttpResponse().result_int(), 200);
+  success = session.handle_http(test_data2, 29);
+  EXPECT_TRUE(success);
+  EXPECT_EQ(session.gethttpResponse().body(), std::string(test_data2, 29));
+  EXPECT_EQ(session.gethttpResponse().result_int(), 200);
+}
