c22b6480fe6da5842c6168a9f6e4858d2e0a4c6d
Shannon Lu <shannonlu78@g.ucla.edu>
2022-04-26 08:21:05 +0000

    * Modified integration testing * Added tests for echo, static file (txt, html, jpg, png, zip), bad requests for local and gcp servers * Created/modified expected files for these tests * Merged with previous commits
    Change-Id: I7593be5a3998bfbd9ccb4ea768e2ec9680a71e60

--- tests/integration_tests.sh
+++ tests/integration_tests.sh
@@ -14,7 +14,12 @@ function run_setup() {
 
 	# generate local server config
 	touch local_test_config
+	echo "server {" > local_test_config
 	echo "listen ${LOCAL_PORT_NUM};" >> local_test_config
+	echo "location /static/help { root /files/images; }" >> local_test_config
+	echo "location /static/ { root /files; }" >> local_test_config
+	echo "location /static2/ { root /files/www; }" >> local_test_config
+	echo "}" >> local_test_config
 }
 
 function run_cleanup() {
@@ -29,24 +34,145 @@ function start_local_server() {
 }
 
 # testing functions
-function local_full_response_test() {
+
+# local server tests
+function local_echo_test() {
 	start_local_server
-	printf 'GET /static/help/index.html HTTP/1.1\r\n\r\n' | nc localhost ${LOCAL_PORT_NUM} > /tmp/actual &
+	printf 'GET /echo HTTP/1.1\r\n\r\n' | nc localhost ${LOCAL_PORT_NUM} > /tmp/actual &
 	sleep 1
 	kill ${local_server_pid} #nc process is killed when the server is killed
-	diff expected_local_full /tmp/actual
+	diff expected_echo /tmp/actual
 }
 
-function gcp_full_response_test() {
-	echo "gcp test 1" | nc ${GCP_IP} ${GCP_PORT_NUM} > /tmp/actual &
+function local_bad_request_test() {
+	start_local_server
+	printf 'GET /echmo HTTP/1.1\r\n\r\n' | nc localhost ${LOCAL_PORT_NUM} > /tmp/actual &
+	sleep 1
+	kill ${local_server_pid} #nc process is killed when the server is killed
+	diff expected_bad_format /tmp/actual
+}
+
+function local_file_not_found_test() {
+	start_local_server
+	printf 'GET /static/help/file1.txt HTTP/1.1\r\n\r\n' | nc localhost ${LOCAL_PORT_NUM} > /tmp/actual &
+	sleep 1
+	kill ${local_server_pid} #nc process is killed when the server is killed
+	diff expected_not_found /tmp/actual
+}
+
+function local_static_txt_test() {
+	start_local_server
+	printf 'GET /static/file1.txt HTTP/1.1\r\n\r\n' | nc localhost ${LOCAL_PORT_NUM} > /tmp/actual &
+	sleep 1
+	kill ${local_server_pid} #nc process is killed when the server is killed
+	diff expected_txt /tmp/actual
+}
+
+function local_static_html_test() {
+	start_local_server
+	printf 'GET /static/index.html HTTP/1.1\r\n\r\n' | nc localhost ${LOCAL_PORT_NUM} > /tmp/actual &
+	sleep 1
+	kill ${local_server_pid} #nc process is killed when the server is killed
+	diff expected_html /tmp/actual
+}
+
+function local_static_zip_test() {
+	start_local_server
+	printf 'GET /static2/empty.zip HTTP/1.1\r\n\r\n' | nc localhost ${LOCAL_PORT_NUM} > /tmp/actual &
+	sleep 1
+	kill ${local_server_pid} #nc process is killed when the server is killed
+	diff expected_zip /tmp/actual
+}
+
+function local_static_jpg_test() {
+	start_local_server
+	printf 'GET /static/help/nyan_cat.jpg HTTP/1.1\r\n\r\n' | nc localhost ${LOCAL_PORT_NUM} > /tmp/actual &
+	sleep 1
+	kill ${local_server_pid} #nc process is killed when the server is killed
+	diff expected_jpg /tmp/actual
+}
+
+function local_static_png_test() {
+	start_local_server
+	printf 'GET /static/help/hutao.png HTTP/1.1\r\n\r\n' | nc localhost ${LOCAL_PORT_NUM} > /tmp/actual &
+	sleep 1
+	kill ${local_server_pid} #nc process is killed when the server is killed
+	diff expected_png /tmp/actual
+}
+
+
+# gcp server tests
+function gcp_echo_test() {
+	printf 'GET /echo HTTP/1.1\r\n\r\n' | nc ${GCP_IP} ${GCP_PORT_NUM} > /tmp/actual &
 	nc_pid=$!
 	sleep 1
 	kill ${nc_pid}
-	diff expected_gcp_full /tmp/actual
+	diff expected_echo /tmp/actual
+}
+
+function gcp_bad_request_test() {
+	printf 'GET /echmo HTTP/1.1\r\n\r\n' | nc ${GCP_IP} ${GCP_PORT_NUM} > /tmp/actual &
+	nc_pid=$!
+	sleep 1
+	kill ${nc_pid}
+	diff expected_bad_format /tmp/actual
+}
+
+function gcp_file_not_found_test() {
+	printf 'GET /static/help/file1.txt HTTP/1.1\r\n\r\n' | nc ${GCP_IP} ${GCP_PORT_NUM} > /tmp/actual &
+	nc_pid=$!
+	sleep 1
+	kill ${nc_pid}
+	diff expected_not_found /tmp/actual
+}
+
+function gcp_static_txt_test() {
+	printf 'GET /static/file1.txt HTTP/1.1\r\n\r\n' | nc ${GCP_IP} ${GCP_PORT_NUM} > /tmp/actual &
+	nc_pid=$!
+	sleep 1
+	kill ${nc_pid}
+	diff expected_txt /tmp/actual
+}
+
+function gcp_static_html_test() {
+	printf 'GET /static/index.html HTTP/1.1\r\n\r\n' | nc ${GCP_IP} ${GCP_PORT_NUM} > /tmp/actual &
+	nc_pid=$!
+	sleep 1
+	kill ${nc_pid}
+	diff expected_html /tmp/actual
+}
+
+function gcp_static_zip_test() {
+	printf 'GET /static2/empty.zip HTTP/1.1\r\n\r\n' | nc ${GCP_IP} ${GCP_PORT_NUM} > /tmp/actual &
+	nc_pid=$!
+	sleep 1
+	kill ${nc_pid}
+	diff expected_zip /tmp/actual
+}
+
+function gcp_static_jpg_test() {
+	printf 'GET /static/help/nyan_cat.jpg HTTP/1.1\r\n\r\n' | nc ${GCP_IP} ${GCP_PORT_NUM} > /tmp/actual &
+	nc_pid=$!
+	sleep 1
+	kill ${nc_pid}
+	diff expected_jpg /tmp/actual
+}
+
+function gcp_static_png_test() {
+	printf 'GET /static/help/hutao.png HTTP/1.1\r\n\r\n' | nc ${GCP_IP} ${GCP_PORT_NUM} > /tmp/actual &
+	nc_pid=$!
+	sleep 1
+	kill ${nc_pid}
+	diff expected_png /tmp/actual
 }
 
 # to add new tests, add name below
-# test_list=(local_full_response gcp_full_response)
+test_list=(local_echo local_bad_request local_file_not_found
+	local_static_txt local_static_html local_static_zip
+	local_static_jpg local_static_png
+	gcp_echo gcp_bad_request gcp_file_not_found
+	gcp_static_txt gcp_static_html gcp_static_zip
+	gcp_static_jpg gcp_static_png)
 # run setup commands
 run_setup
 
