053ce17de61e4a2db3a70cc0c6b6d064d03fd208
keshavc217 <keshavc217@g.ucla.edu>
2022-05-14 18:29:39 -0700

    This commit handles POST requests + refactoring from previous commit
    Change-Id: I7df0b626edaeda24da333d62ad5db4faefb0db6f

--- include/api_handler.h
+++ include/api_handler.h
@@ -6,11 +6,11 @@
 class ApiHandler: public RequestHandler 
 {
     public:
-        ApiHandler(std::string location, std::string root_file_path);
+        ApiHandler(std::string location, std::string data_path);
         bool handle_request(http::request<http::string_body> httpRequest, http::response<http::string_body>& httpResponse);
     private:
+        std::string data_path_;
         std::string location_;
-        std::string root_file_path_;
 };
 
 #endif
--- src/api_handler.cc
+++ src/api_handler.cc
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <fstream>
 
 #include "request_handler.h"
 #include "api_handler.h"
@@ -11,11 +12,11 @@
 
 using namespace std;
 
-ApiHandler::ApiHandler(std::string location, std::string root_file_path)
-    :location_(location), root_file_path_(root_file_path){}
+ApiHandler::ApiHandler(std::string location, std::string data_path)
+    :location_(location), data_path_(data_path){}
 
 // split function helps break up the uri into more manageable parts
-vector<string> split (const string &s, char delim) 
+vector<string> split(const string &s, char delim) 
 {
     vector<string> result;
     stringstream ss (s);
@@ -25,7 +26,57 @@ vector<string> split (const string &s, char delim)
         result.push_back (item);
     }
 
+    // remove all empty strings
+    result.erase(std::remove(result.begin(), result.end(), ""), result.end());
+    result.erase(result.begin()); // remove the api
+
     return result;
+}
+
+std::string extractEntity(vector<string> uri) 
+{
+    return "/" + uri[0];
+}
+
+int getUniqueFileName(boost::filesystem::path p) 
+{
+    int largest_file = 0; // store index of largest file in directory
+    for (const auto &entry : boost::make_iterator_range(boost::filesystem::directory_iterator(p), {}))
+    {
+        std::string curr_file_name = entry.path().stem().string();
+        int curr_file_num = stoi(curr_file_name);
+        if (curr_file_num > largest_file)
+        {
+            largest_file = curr_file_num;
+        }
+    }
+
+    // Now that we have the largest file number in the directory, add 1 and add to file path
+    return largest_file + 1;
+}
+
+// Creates file and returns the file number created
+int handlePost(http::request<http::string_body> httpRequest, vector<std::string> uri)
+{
+    std::string filePath = data_path_ + extractEntity(uri);
+    std::string postBody = httpRequest.body();
+    
+        
+    boost::filesystem::path p(filePath);
+    if (boost::filesystem::exists(p) == false)
+    {
+        boost::filesystem::create_directory(p);
+    }
+        
+    int created_file = getUniqueFileName(p);
+
+    filePath = filePath + "/" + std::to_string(created_file);
+    ofstream myfile;
+    myfile.open(filePath);
+    myfile << postBody;
+    myfile.close();
+
+    return created_file;
 }
 
 
@@ -39,14 +90,21 @@ bool ApiHandler::handle_request(http::request<http::string_body> httpRequest, ht
     // switch action based on method
     // target is the URI we need
 
-    // /api/Shoes --> {,api,shoes} (note that size is 3 not 2)
+    // /api/Shoes --> {shoes in our uri} 
     vector<string> uri = split(target,'/');
 
     if (method == "POST") 
     {
         // CREATE
-
-
+        if (uri.size() != 1)
+        {
+            LOG(error) << "POST request has more than one entity, please post to /api/{entity} only.";
+            set_response(http::status::bad_request, "text/plain", "Not a valid API request target for POST\n", httpResponse);
+            return false;
+        }
+        int created_file_number = handlePost(httpRequest, uri);
+        std::string json_response = "{\"id\": " + to_string(created_file_number) + "}";
+        set_response(http::status::ok, "text/plain", json_response, httpResponse);
     }
     else if (method == "GET") 
     {
