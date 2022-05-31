dab31233e1e4a2b4b7c8ecbdfe1a44a9ac9f28c4
Shannon Lu <shannonlu78@g.ucla.edu>
2022-05-31 06:08:49 +0000

    Implemented get_names
    * Added json parsing helper method in palette_api.cc
    * Added boost dependencies to CMakeLists as necessary
    
    Change-Id: I2c56b14ffa19232538dbb9d57488cfe27ac804c3

--- src/palette_api.cc
+++ src/palette_api.cc
@@ -2,6 +2,7 @@
 #include "palette_api.h"
 #include "logger.h"
 
+#include <algorithm>
 #include <string>
 #include <sstream>
 #include <iostream>
@@ -9,11 +10,25 @@
 #include <curlpp/cURLpp.hpp>
 #include <curlpp/Easy.hpp>
 #include <curlpp/Options.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
 #define HEX_START 22
 #define HEX_LENGTH 7
 
 using namespace curlpp::options;
+namespace pt = boost::property_tree;
+
+// Source: https://stackoverflow.com/questions/43725302/reading-json-with-boost-property-tree
+std::string parse_json(std::string json)
+{
+    std::stringstream ss;
+    ss << json;
+
+    pt::ptree json_tree;
+    pt::read_json(ss, json_tree);
+    const pt::ptree& name = json_tree.get_child("name");
+    return name.get<std::string>("value");
+}
 
 // TODO: currently only returns one color
 std::vector<std::string> PaletteAPI::get_colors(std::string word)
@@ -50,8 +65,35 @@ std::vector<std::string> PaletteAPI::get_colors(std::string word)
     return colors;
 }
 
-// TODO: dummy implementation for now
+// TODO: currently outputs json, will need to be parsed
 std::vector<std::string> PaletteAPI::get_names(std::vector<std::string> hexColors)
 {
-    return hexColors;
+    std::vector<std::string> colorNames;
+    try
+    {
+        curlpp::Cleanup myCleanup;
+
+        for (int i = 0; i < hexColors.size(); i++)
+        {
+            std::ostringstream url;
+            url << "https://www.thecolorapi.com/id?hex=" << hexColors[i].substr(1);
+
+            std::ostringstream os;
+            os << Url(url.str());
+
+            // Extract name from web page
+            std::string name = parse_json(os.str());
+            colorNames.push_back(name);
+        }
+    }
+    catch (curlpp::RuntimeError &e)
+    {
+        LOG(error) << "While getting name of colors: " << e.what() << "\n";
+    }
+    catch (curlpp::LogicError &e)
+    {
+        LOG(error) << "While getting name of colors: " << e.what() << "\n";
+    }
+
+    return colorNames;
 }
